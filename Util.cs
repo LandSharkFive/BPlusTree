ee23a19f3e3d1ad4356de7882b38a020d1c52de0
LandSharkFive <153857026+LandSharkFive@users.noreply.github.com>
2023-12-14 13:28:55 -0500

    Add files via upload
    A B+ Tree with Unit Testing.

---
+++ Util.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace BPTestOne
+{
+    public static class Util
+    {
+        public static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void PrintArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public static bool IsSorted(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] > array[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsSorted(double[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] > array[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
